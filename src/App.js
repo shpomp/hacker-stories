bd9d80a1c2a1126d93ebd3926025fa5f5abe0bf2
shpomp <kur@dixa.com>
2021-10-25 11:31:12 +0200

    spread and rest 66

--- src/App.js
+++ src/App.js
@@ -12,15 +12,8 @@ const Search = ({ search, onSearch }) => {
 
 const List = ({ list }) => (
 	<ul>
-		{list.map((item) => (
-			<Item
-				key={item.objectID}
-				title={item.title}
-				url={item.url}
-				author={item.author}
-				num_comments={item.num_comments}
-				points={item.points}
-			/>
+		{list.map(({ objectID, ...item }) => (
+			<Item key={objectID} {...item} />
 		))}
 	</ul>
 );
