69b8173acdeb34410a117b0cb11d21afea0cf998
shpomp <kur@dixa.com>
2021-10-27 13:29:34 +0200

    svg 156

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import styles from "../App.module.css";
 import styled from "styled-components";
+import { ReactComponent as Check } from "../check.svg";
 
 // -------------- STYLED COMPONENTS --------------
 
@@ -49,7 +50,7 @@ const Item = ({ item, onRemoveItem }) => {
 			<StyledColumn width="10%">{item.points}</StyledColumn>
 			<StyledColumn width="10%">
 				<StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
-					Dismiss
+					<Check height="18px" width="18px" />
 				</StyledButtonSmall>
 			</StyledColumn>
 		</StyledItem>
