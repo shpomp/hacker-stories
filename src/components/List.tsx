6566c2e1c6d1e255ab336dbdb909cc3d4fe833c2
shpomp <kur@dixa.com>
2021-11-02 16:06:12 +0100

    sorting book way + 215

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,25 +3,29 @@ import Item from "./Item";
 import { sortBy, orderBy } from "lodash";
 import type { Story, Stories } from "../App";
 
+// -------------- TYPES --------------
+
 type ListProps = {
 	list: Stories;
 	onRemoveItem: (item: Story) => void;
 };
-// type SortsType = {
-// 	[NONE: string]: (list: Stories) => Stories;
-// 	TITLE: (list: Stories) => Stories;
-// 	AUTHOR: (list: Stories) => Stories;
-// 	COMMENT: (list: Stories) => Stories;
-// 	POINT: (list: Stories) => Stories;
-// };
 
-// const SORTS: SortsType = {
-// 	NONE: (list: Stories): Stories => list,
-// 	TITLE: (list: Stories): Stories => sortBy(list, "title"),
-// 	AUTHOR: (list: Stories): Stories => sortBy(list, "author"),
-// 	COMMENT: (list: Stories): Stories => sortBy(list, "num_comments").reverse(),
-// 	POINT: (list: Stories): Stories => sortBy(list, "points").reverse(),
-// };
+type SortsType = {
+	[NONE: string]: (list: Stories) => Stories;
+	TITLE: (list: Stories) => Stories;
+	AUTHOR: (list: Stories) => Stories;
+	COMMENT: (list: Stories) => Stories;
+	POINT: (list: Stories) => Stories;
+};
+
+const SORTS: SortsType = {
+	NONE: (list: Stories): Stories => list,
+	TITLE: (list: Stories): Stories => sortBy(list, "title"),
+	AUTHOR: (list: Stories): Stories =>
+		orderBy(list, [(item) => item.author.toLowerCase()], ["asc"]),
+	COMMENT: (list: Stories): Stories => sortBy(list, "num_comments").reverse(),
+	POINT: (list: Stories): Stories => sortBy(list, "points").reverse(),
+};
 
 // -------------- COMPONENT --------------
 
@@ -32,26 +36,26 @@ const List = ({ list, onRemoveItem }: ListProps) => {
 		setSort(sortKey);
 	};
 
-	const sortList = (sort: string, list: Stories): Stories => {
-		switch (sort) {
-			case "NONE":
-				return list;
-			case "TITLE":
-				return sortBy(list, "title");
-			case "AUTHOR":
-				return orderBy(list, [(item) => item.author.toLowerCase()], ["asc"]);
-			case "COMMENT":
-				return sortBy(list, "num_comments").reverse();
-			case "POINT":
-				return sortBy(list, "points").reverse();
-			default:
-				return list;
-		}
-	};
-	const sortedList = sortList(sort, list);
+	// const sortList = (sort: string, list: Stories): Stories => {
+	// 	switch (sort) {
+	// 		case "NONE":
+	// 			return list;
+	// 		case "TITLE":
+	// 			return sortBy(list, "title");
+	// 		case "AUTHOR":
+	// 			return orderBy(list, [(item) => item.author.toLowerCase()], ["asc"]);
+	// 		case "COMMENT":
+	// 			return sortBy(list, "num_comments").reverse();
+	// 		case "POINT":
+	// 			return sortBy(list, "points").reverse();
+	// 		default:
+	// 			return list;
+	// 	}
+	// };
+	// const sortedList = sortList(sort, list);
 
-	// const sortFunction = SORTS[sort];
-	// const sortedList = sortFunction(list);
+	const sortFunction = SORTS[sort];
+	const sortedList = sortFunction(list);
 
 	return (
 		<ul className="list">
